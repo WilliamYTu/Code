1439beae69b1cb706c3301fc70bce63a52b74200
willi_zklcjzk <wyt4148.t@gmail.com>
2023-02-01 21:47:53 -0600

    hi

--- Trip5-2-1-23.py
+++ Trip5-2-1-23.py
@@ -868,24 +868,17 @@ def trip_4():
     StraightPID_right(-75, -17, 35)
     
     # # approach to water reservoir
-    Turn_l(-22)
-    StraightPID_right(-22, 19, 30)
-    
-    # Turn_r(-30)
-    # StraightPID_right(-30, 6, 30)
-    # #motor_pair.move_tank(2, "seconds", 15, 15)
-    # #wait_for_seconds(0.5)
-    
-    
-    StraightPID_right(-20, -24, 35)
-    Turn_r(-92)
-    StraightPID_right(-92, 23, 40)
+    Turn_l(-25)
+    StraightPID_right(-25, 21, 30)
+    StraightPID_right(-25, -20, 35)
+    Turn(-88)
+    StraightPID_right(-88, 23, 40)
    
     #StraightPID_right(-92, -1, 30)
     
     #Turn(-98)
    
-    StraightPID_left(-115, -40, 70)
+    StraightPID_left(-105, -40, 70)
 def trip_5():
     StraightPID_left(0, 80, 50)
     Turn(45)
@@ -893,6 +886,17 @@ def trip_5():
     Turn(-45)
     #StraightPID_left(-45, 2, 30)
 
+wait()
+initialize()
+trip_inprogress()
+trip_3()
+done()
+
+wait()
+initialize()
+trip_inprogress()
+trip_4()
+done()  
 
 wait()
 initialize()
